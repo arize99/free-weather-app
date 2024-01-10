ce1d2a130548160510be5f4e46d63c93f94880d3
jameydev <jybryce4@gmail.com>
2024-01-10 17:07:08 -0600

    brought in Body and Info components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
+import Body from './components/weather_body/Body';
 import Header from './components/weather_header/Header';
+import Info from './components/weather_info/Info';
 
 export default function App() {
     return (
         <>
             <Header />
+            <Body />
+            <Info />
         </>
     );
 }
