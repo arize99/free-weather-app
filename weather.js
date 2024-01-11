425166432021210fddc47629a5a43ba4aeb8d4ba
arize <aristotlejpg@yahoo.com>
2024-01-11 13:33:04 +0100

    Okay, I had to finish up the Weather App. I was very impressed with the attention and support this repo generatred. I had to set the default city to my home town Awka. But there is still a large position open for a lot of CSS and JS prompts to make the site look more alive.

--- weather.js
+++ weather.js
@@ -1,38 +1,100 @@
 // File: weather.js
 // Author: Jamey Bryce
+// Contributor: Arize Nnonyelu
 // Purpose: Frontend JS
 // for displaying weather
 
-// weather units
-const weatherUnits = document.querySelector('.weather__units');
-const celsius = document.querySelector('.weather_unit_celsius');
-const fahrenheit = document.querySelector('.weather_unit_fahrenheit');
+// state
+let currCity = "Awka";
+let units = "metric";
 
-// grab the weather body and children
-const weatherBody = document.querySelector('.weather__body');
-const weatherIcon = document.querySelector('.weather__icon');
-const weatherForecast = document.querySelector('.weather__forecast');
-const weatherDatetime = document.querySelector('.weather__datetime');
-const weatherTemp = document.querySelector('.weather__temperature');
-const weatherMinMax = document.querySelector('.weather__minmax');
+// Selectors
+let city = document.querySelector(".weather__city");
+let datetime = document.querySelector(".weather__datetime");
+let weather__forecast = document.querySelector('.weather__forecast');
+let weather__temperature = document.querySelector(".weather__temperature");
+let weather__icon = document.querySelector(".weather__icon");
+let weather__minmax = document.querySelector(".weather__minmax")
+let weather__realfeel = document.querySelector('.weather__realfeel');
+let weather__humidity = document.querySelector('.weather__humidity');
+let weather__wind = document.querySelector('.weather__wind');
+let weather__pressure = document.querySelector('.weather__pressure');
 
-// grab the weather info and children
-const weatherInfo = document.querySelector('.weather__info');
-const weatherCity = document.querySelectorAll('.weather__city');
-const weatherCards = document.querySelectorAll('.weather__card');
+// search
+document.querySelector(".weather__search").addEventListener('submit', e => {
+    let search = document.querySelector(".weather__searchform");
+    // prevent default action
+    e.preventDefault();
+    // change current city
+    currCity = search.value;
+    // get weather forecast 
+    getWeather();
+    // clear form
+    search.value = ""
+})
 
-// humidity, wind, pressure
-const weatherHumidity = document.querySelector('.weather__humidity');
-const weatherWind = document.querySelector('.weather__wind');
-const weatherPressure = document.querySelector('.weather__pressure');
+// units
+document.querySelector(".weather_unit_celsius").addEventListener('click', () => {
+    if(units !== "metric"){
+        // change to metric
+        units = "metric"
+        // get weather forecast 
+        getWeather()
+    }
+})
 
-// the above is a good start,
-// but we need to get the data
-// from the API and put it in
-// the fields we just grabbed.
-// I'll push this for now and
-// see if anybody wants to get
-// a go at the API before I do
-// ************************
-// TO-DO: pull actual data from OpenWeatherMap API
-// and display it on the page
+document.querySelector(".weather_unit_farenheit").addEventListener('click', () => {
+    if(units !== "imperial"){
+        // change to imperial
+        units = "imperial"
+        // get weather forecast 
+        getWeather()
+    }
+})
+
+function convertTimeStamp(timestamp, timezone){
+     const convertTimezone = timezone / 3600; // convert seconds to hours 
+
+    const date = new Date(timestamp * 1000);
+    
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        timeZone: `Etc/GMT${convertTimezone >= 0 ? "-" : "+"}${Math.abs(convertTimezone)}`,
+        hour12: true,
+    }
+    return date.toLocaleString("en-US", options)
+   
+}
+
+ 
+
+// convert country code to name
+function convertCountryCode(country){
+    let regionNames = new Intl.DisplayNames(["en"], {type: "region"});
+    return regionNames.of(country)
+}
+
+function getWeather(){
+    const API_KEY = '64f60853740a1ee3ba20d0fb595c97d5'
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currCity}&appid=${API_KEY}&units=${units}`).then(res => res.json()).then(data => {
+    console.log(data)
+    city.innerHTML = `${data.name}, ${convertCountryCode(data.sys.country)}`
+    datetime.innerHTML = convertTimeStamp(data.dt, data.timezone); 
+    weather__forecast.innerHTML = `<p>${data.weather[0].main}`
+    weather__temperature.innerHTML = `${data.main.temp.toFixed()}&#176`
+    weather__icon.innerHTML = `   <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" />`
+    weather__minmax.innerHTML = `<p>Min: ${data.main.temp_min.toFixed()}&#176</p><p>Max: ${data.main.temp_max.toFixed()}&#176</p>`
+    weather__realfeel.innerHTML = `${data.main.feels_like.toFixed()}&#176`
+    weather__humidity.innerHTML = `${data.main.humidity}%`
+    weather__wind.innerHTML = `${data.wind.speed} ${units === "imperial" ? "mph": "m/s"}` 
+    weather__pressure.innerHTML = `${data.main.pressure} hPa`
+})
+}
+
+document.body.addEventListener('load', getWeather())
