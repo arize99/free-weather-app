36fba94776a9fcc086cf044e70b3aa2126d64aa3
cyberz3r0 <reidsl0716@gmail.com>
2024-01-11 13:05:57 -0800

    removed orphans

--- weather.js
+++ weather.js
@@ -5,16 +5,9 @@
 // for displaying weather
 
 // search form, search input, city, humidity, wind, pressure
-let city = document.querySelector(".weather__city");
 let datetime = document.querySelector(".weather__datetime");
 let weather__forecast = document.querySelector('.weather__forecast');
-let weather__temperature = document.querySelector(".weather__temperature");
 let weather__icon = document.querySelector(".weather__icon");
-let weather__minmax = document.querySelector(".weather__minmax")
-let weather__realfeel = document.querySelector('.weather__realfeel');
-let weather__humidity = document.querySelector('.weather__humidity');
-let weather__wind = document.querySelector('.weather__wind');
-let weather__pressure = document.querySelector('.weather__pressure');
 const weatherSearch = document.querySelector('.weather__search');
 const weatherSearchInput = document.querySelector('.weather__searchinput');
 const weatherCity = document.querySelector('.weather__city');
